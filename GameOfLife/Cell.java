32418e19079dfb82b8c1f05a52747be01e4ddd84
magnitopic <magnitopic@gmail.com>
2021-10-26 13:35:28 +0200

    You can get cell's neighbours

--- GameOfLife/Cell.java
+++ GameOfLife/Cell.java
@@ -2,64 +2,46 @@ package GameOfLife;
 
 import java.util.ArrayList;
 
-public class Cell{
-  public boolean alive;
-  public int x;
-  public int y;
-  public ArrayList<Cell> neighbours;
+public class Cell {
+	public boolean alive;
+	public int x;
+	public int y;
+	public ArrayList<Cell> neighbours;
 
-  public Cell(boolean alive, int x, int y) {
-    this.alive = alive;
-    this.x = x;
-    this.y = y;
-  }
+	public Cell(boolean alive, int x, int y) {
+		this.alive = alive;
+		this.x = x;
+		this.y = y;
+		this.neighbours = new ArrayList<Cell>();
+	}
 
-  public void getNeighbours(ArrayList<ArrayList<Cell>> board) {
-    for(int i=0;i<29;i++){
-			for(int j=0;j<29;j++){
-        switch(board.get(i).get(j).x){
-          case x:
-            switch(board.get(i).get(j).y){
-              case y-1:
-                neighbours.add(board.get(i).get(j));
-              break;
-              case y+1:
-                neighbours.add(board.get(i).get(j));
-              break;
-            }
-          break;
-          case x-1:
-            switch(board.get(i).get(j).y){
-              case y:
-                neighbours.add(board.get(i).get(j));
-              break;
-              case y-1:
-                neighbours.add(board.get(i).get(j));
-              break;
-              case y+1:
-                neighbours.add(board.get(i).get(j));
-              break;
-            }
-          break;
-          case x+1:
-            switch(board.get(i).get(j).y){
-              case y:
-                neighbours.add(board.get(i).get(j));
-              break;
-              case y-1:
-                neighbours.add(board.get(i).get(j));
-              break;
-              case y+1:
-                neighbours.add(board.get(i).get(j));
-              break;
-            }
-          break;
-        }
+	public void getNeighbours(ArrayList<ArrayList<Cell>> board) {
+		for (int i = 0; i < board.size(); i++) {
+			for (int j = 0; j < board.get(0).size(); j++) {
+				if ((board.get(i).get(j).x == this.x) || (board.get(i).get(j).x == (this.x - 1))
+						|| (board.get(i).get(j).x == (this.x + 1))) {
+					if ((board.get(i).get(j).y == (this.y - 1)) || (board.get(i).get(j).y == (this.y + 1))
+					// I have to make sure I don't add this cell, only it's neighbours
+					// So I make sure that both X and Y aren't the same
+							|| ((board.get(i).get(j).y == this.y) && (board.get(i).get(j).x != this.x))) {
+						this.neighbours.add(board.get(i).get(j));
+					}
+				}
 			}
 		}
-  }
+	}
 
-  public void newGeneration() {
-
-  }
+	public void newGeneration() {
+		int counter = 0;
+		for (int i = 0; i < neighbours.size(); i++) {
+			if (neighbours.get(i).alive) {
+				counter++;
+			}
+		}
+		if (counter < 2 || counter < 3) {
+			this.alive = false;
+		} else if (counter == 3) {
+			this.alive = true;
+		}
+	}
 }
--- GameOfLife/Game.java
+++ GameOfLife/Game.java
@@ -1,45 +1,69 @@
 package GameOfLife;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
-public class Game{
+public class Game {
 	public ArrayList<ArrayList<Cell>> board;
-	
+
 	public Game() {
-		this.board=new ArrayList<ArrayList<Cell>>();
+		this.board = new ArrayList<ArrayList<Cell>>();
 	}
-	
+
 	public void newGame() {
-		for(int i=0;i<29;i++){
-			ArrayList<Cell> row=new ArrayList<Cell>();
-			for(int j=0;j<29;j++){
-				Cell cell=new Cell(false,i,j);
+		for (int i = 0; i < 29; i++) {
+			ArrayList<Cell> row = new ArrayList<Cell>();
+			for (int j = 0; j < 29; j++) {
+				Cell cell = new Cell(false, i, j);
 				row.add(cell);
 			}
 			this.board.add(row);
 		}
+		setNeighbours();
 	}
 
-	public void printBoard() {
-		for(int i=0;i<29;i++){
-			System.out.println("");
-			for(int j=0;j<29;j++){
-				if(board.get(i).get(j).alive){
-					System.out.print(" ■ ");
-				}else{
-					System.out.print(" □ ");
-				}
+	public void setNeighbours() {
+		for (int i = 0; i < board.size(); i++) {
+			for (int j = 0; j < board.get(0).size(); j++) {
+				board.get(i).get(j).getNeighbours(this.board);
 			}
 		}
 	}
 
+	public void printBoard() {
+		for (int i = 0; i < board.size(); i++) {
+			System.out.println("");
+			for (int j = 0; j < board.get(0).size(); j++) {
+				if (board.get(i).get(j).alive) {
+					System.out.print(" ■ ");
+				} else {
+					System.out.print(" □ ");
+				}
+			}
+		}
+		System.out.println("");
+		System.out.print("____________________");
+	}
+
+	public void newGeneration() {
+		
+	}
+
+
+	//Test method that let's you see any 
 	public void test() {
-		/* board.get(5).get(5).getNeighbours(board); */
+		Cell testCell=board.get(14).get(14);
+
+		for(int i=0;i<testCell.neighbours.size();i++){
+			testCell.neighbours.get(i).alive=true;
+		}
 	}
 
 	public static void main(String[] args) {
-		Game game=new Game();
+		Game game = new Game();
 		game.newGame();
+		game.printBoard();
+		game.test();
 		game.printBoard();
 	}
 }
