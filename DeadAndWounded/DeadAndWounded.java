5a570009c8910f50c77e58f024cd8eb1afdc6629
magnitopic <magnitopic@gmail.com>
2021-11-22 18:58:17 +0100

    New method, startGame

--- DeadAndWounded/DeadAndWounded.java
+++ DeadAndWounded/DeadAndWounded.java
@@ -29,11 +29,34 @@ public class DeadAndWounded {
 		}
 	}
 
+	public void turn(Scanner scanner) {
+		
+	}
+
+	public void startGame() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("============= Hurt and Wounded Game =============");
+		System.out.println("I'm thinking of four diferent numbers from cero to nine, bouth included.");
+		System.out.println("If you correctlly guess the number but not it's position it will be counted as wounded");
+		System.out.println("If you guess bouth the number and it's position it will be counted as dead.");
+		System.out.println("You must get four dead to win. Good luck.");
+		System.out.println("==========================");
+		boolean playing=true;
+		while(playing){
+			turn(scanner);
+			trys--;
+			if(trys<=0){
+				System.out.println("You have run out of trys. You lose.");
+				playing=flase;
+			}else if(){
+				System.out.println("You win! You guessed "+num.get(0)+num.get(1)+num.get(2)+num.get(3)+" in "+this.trys+" trys.");
+				playing=flase;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		DeadAndWounded dead=new DeadAndWounded();
-		for(int i=0;i<=3;i++){
-			System.out.println(dead.num.get(i));
-		}
 
 		
 	}
