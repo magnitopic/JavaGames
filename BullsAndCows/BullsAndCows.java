e5945c1c1c11186395b3a4b36b1a1708b48a15b5
magnitopic <magnitopic@gmail.com>
2021-12-28 17:32:42 +0100

    Better names

--- BullsAndCows/BullsAndCows.java
+++ BullsAndCows/BullsAndCows.java
@@ -1,14 +1,14 @@
-package Bulls and Cows;
+package BullsAndCows;
 
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Bulls and Cows {
+public class BullsAndCows {
 	private int trys;
 	public ArrayList<Integer> num;
 	private ArrayList<Integer> userNum;
 
-	public Bulls and Cows() {
+	public BullsAndCows() {
 		this.trys = 15;
 		this.num = new ArrayList<Integer>();
 		int i = 0;
@@ -17,7 +17,7 @@ public class Bulls and Cows {
 			int rnd = (int) (Math.random() * 10);
 			boolean diferent = true;
 
-			for (int j = 0; j <num.size(); j++) {
+			for (int j = 0; j < num.size(); j++) {
 				if (num.get(j) == rnd) {
 					diferent = false;
 				}
@@ -39,16 +39,16 @@ public class Bulls and Cows {
 	// I added the condition that the number has to have 4 unique digits
 	public void isInputInt(Scanner scanner) {
 		while (true) {
-			String input=scanner.nextLine();
-			if(input.length()>4 && input.length()<4){
-				
+			String input = scanner.nextLine();
+			if (input.length() > 4 && input.length() < 4) {
+
 			}
 		}
 	}
 
 	public void startGame() {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("============= Bulls and Cows Game =============");
+		System.out.println("============= BullsAndCows Game =============");
 		System.out.println("I'm thinking of four diferent digits from cero to nine, bouth included.");
 		System.out.println("If you correctlly guess the value of a digit but not it's position it will be counted Cow");
 		System.out.println("If you guess bouth the digits value and it's position it will be counted as Bull.");
@@ -60,18 +60,17 @@ public class Bulls and Cows {
 			trys--;
 			if(trys<=0){
 				System.out.println("You have run out of trys. You lose.");
-				playing=flase;
+				playing=false;
 			}else if(){
 				System.out.println("You win! You guessed "+num.get(0)+num.get(1)+num.get(2)+num.get(3)+" in "+this.trys+" trys.");
-				playing=flase;
+				playing=false;
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		Bulls and Cows dead=new Bulls and Cows();
+		BullsAndCows dead = new BullsAndCows();
 
-		
 	}
 
 }
