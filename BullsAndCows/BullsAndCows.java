a1f1b344b1b690bb34f5117fda24480ff73c8ce6
magnitopic <magnitopic@gmail.com>
2022-01-01 23:35:36 +0100

    Finished Bulls and Cows

--- BullsAndCows/BullsAndCows.java
+++ BullsAndCows/BullsAndCows.java
@@ -1,76 +1,118 @@
 package BullsAndCows;
 
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class BullsAndCows {
-	private int trys;
-	public ArrayList<Integer> num;
-	private ArrayList<Integer> userNum;
 
-	public BullsAndCows() {
-		this.trys = 15;
-		this.num = new ArrayList<Integer>();
-		int i = 0;
-		// Generates 4 dieferent Integers for num
-		while (i < 4) {
-			int rnd = (int) (Math.random() * 10);
-			boolean diferent = true;
+  private int trys;
+  public String num;
+  private String userNum;
+  public int bulls;
+  public int cows;
 
-			for (int j = 0; j < num.size(); j++) {
-				if (num.get(j) == rnd) {
-					diferent = false;
-				}
-			}
-			if (diferent) {
-				i++;
-				this.num.add(rnd);
-			}
-		}
-	}
+  public BullsAndCows() {
+    this.trys = 15;
+    // Generates 4 dieferent Integers for num
+    while (true) {
+      String rnd = "";
+      for (int i = 0; i < 4; i++) {
+        rnd += String.valueOf((int) (Math.random() * 10));
+      }
+      boolean diferent = diferenceChecker(rnd);
+      if (diferent) {
+        this.num = rnd;
+        break;
+      }
+    }
+  }
 
-	public void turn(Scanner scanner) {
-		System.out.println("Give me a number with four diferent digits: ");
-		isInputInt(scanner);
-	}
+  // This function checks that the user input is a number and will ask until it
+  // gets one. Made it to simplify the code that deals with the user.
+  // I added the condition that the number has to have 4 unique digits
+  private String getUserInput(Scanner scanner) {
+    while (true) {
+      System.out.print("Give me a number with four diferent digits: ");
+      String input = scanner.nextLine();
+      if (input.length() == 4 && diferenceChecker(input)) {
+        return input;
+      } else {
+        System.out.println("Invalid input. Try again.");
+      }
+    }
+  }
 
-	// This function checks that the user input is a number and will ask until it
-	// gets one. Made it to simplify the code that deals with the user.
-	// I added the condition that the number has to have 4 unique digits
-	public void isInputInt(Scanner scanner) {
-		while (true) {
-			String input = scanner.nextLine();
-			if (input.length() > 4 && input.length() < 4) {
+  private void checkBullsCows() {
+    // Checks the bulls and cows
+    // If the userNum is in the same position as num it adds a Bull
+    // Else it checks if userNum is in some other position, if so it adds a Cow
 
-			}
-		}
-	}
+    this.cows = 0;
+    this.bulls = 0;
+    for (int i = 0; i < num.length(); i++) {
+      if (userNum.charAt(i) == num.charAt(i)) {
+        bulls++;
+      } else {
+        for (int j = 0; j < num.length(); j++) {
+          if (userNum.charAt(i) == num.charAt(j)) {
+            cows++;
+          }
+        }
+      }
+    }
+  }
 
-	public void startGame() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("============= BullsAndCows Game =============");
-		System.out.println("I'm thinking of four diferent digits from cero to nine, bouth included.");
-		System.out.println("If you correctlly guess the value of a digit but not it's position it will be counted Cow");
-		System.out.println("If you guess bouth the digits value and it's position it will be counted as Bull.");
-		System.out.println("You must get four Bulls to win and you have 15 trys. Good luck.");
-		System.out.println("==========================");
-		boolean playing=true;
-		while(playing){
-			turn(scanner);
-			trys--;
-			if(trys<=0){
-				System.out.println("You have run out of trys. You lose.");
-				playing=false;
-			}else if(){
-				System.out.println("You win! You guessed "+num.get(0)+num.get(1)+num.get(2)+num.get(3)+" in "+this.trys+" trys.");
-				playing=false;
-			}
-		}
-	}
+  private boolean diferenceChecker(String number) {
+    boolean diferent = true;
 
-	public static void main(String[] args) {
-		BullsAndCows dead = new BullsAndCows();
+    for (int j = 0; j <= number.length() - 1; j++) {
+      for (int k = j + 1; k <= number.length() - 1; k++) {
+        if (number.charAt(j) == number.charAt(k)) {
+          diferent = false;
+        }
+      }
+    }
+    return diferent;
+  }
 
-	}
+  public void startGame() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("============= BullsAndCows Game =============");
+    System.out.println(
+      "I'm thinking of four diferent digits from cero to nine, bouth included."
+    );
+    System.out.println(
+      "If you correctlly guess the value of a digit but not it's position it will be counted Cow"
+    );
+    System.out.println(
+      "If you guess bouth the digits value and it's position it will be counted as Bull."
+    );
+    System.out.println(
+      "You must get four Bulls to win and you have 15 trys. Good luck."
+    );
+    System.out.println("==========================");
+    boolean playing = true;
+    System.out.println(this.num);
+    while (playing) {
+      this.userNum = getUserInput(scanner);
+      trys--;
+      checkBullsCows();
+      System.out.println("Bulls: " + this.bulls + " Cows: " + this.cows);
+      System.out.println("You have " + this.trys + " trys left.");
+      System.out.println("==========================");
+      if (this.num.equals(userNum)) {
+        System.out.println(
+          "You win! You guessed " + num + " in " + (15 - this.trys) + " trys."
+        );
+        playing = false;
+      } else if (trys <= 0) {
+        System.out.println("You have run out of trys. You lose.");
+        playing = false;
+      }
+    }
+  }
 
+  public static void main(String[] args) {
+    BullsAndCows dead = new BullsAndCows();
+    dead.startGame();
+  }
 }
--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Games made in Java.
 
 ## Available games:
 
-| Name                | Date       |
-| ------------------- | ---------- |
-| Rock Paper Scissors | 2021-07-21 |
-| Last Stone          | 2021-08-28 |
-| Set Zeroes          | 2021-08-30 |
-| Guess The Number    | 2021-08-10 |
-| Two Number Target   | 2021-08-13 |
-|Tree|2021-10-14 |
-|Game Of Life|2021-11-1|
-|Bulls and Cows|In progress|
-|Herds|In progress|
+| Name                | Date        |
+| ------------------- | ----------- |
+| Rock Paper Scissors | 2021-07-21  |
+| Last Stone          | 2021-08-28  |
+| Set Zeroes          | 2021-08-30  |
+| Guess The Number    | 2021-08-10  |
+| Two Number Target   | 2021-08-13  |
+| Tree                | 2021-10-14  |
+| Game Of Life        | 2021-11-1   |
+| Bulls and Cows      | 2022-01-01  |
+| Herds               | In progress |
